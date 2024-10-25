876b5965ec0de0a07f4c3aa2f2db1c877cb2d0ac
Andy Gomez <45979588+Koato@users.noreply.github.com>
2024-10-25 19:20:42 +0200

    first commit

---
+++ src/main/java/com/edu/cibertec/efsrt/sanowise/SanowiseApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.efsrt.sanowise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanowiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanowiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/cibertec/efsrt/sanowise/SanowiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.efsrt.sanowise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanowiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
